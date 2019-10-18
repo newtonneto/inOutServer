caaf61c2c0807c5063f7ba59ccd16f49e4cc568a
newtonneto <newton.mgs@gmail.com>
2019-10-18 14:25:49 -0300

    #Alpha 2.4.3
    -Possibilita gerenciamento de processos através do Admin

--- inout/admin.py
+++ inout/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Documento #, Usuario
+from .models import Documento, Processo
 
 # Register your models here.
 admin.site.register(Documento)
-#admin.site.register(Usuario)
+admin.site.register(Processo)
